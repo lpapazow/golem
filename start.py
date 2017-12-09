91fa51bf7488f7d20852ee716250178e028b9c51
Maria Rangelova <maria.rangelova93@gmail.com>
2017-12-09 19:37:59 +0200

    Create dir for json data if it doesn't exist

--- start.py
+++ start.py
@@ -1,11 +1,15 @@
+import os
 from golem.extractors import basicextractor
 from golem.parsers import csvtojson
 from golem.extractors import tweetcontainstagextractor
 
 def main():
     parser = csvtojson.CsvToJsonParser('data/data_csv/2018-E-c-En-train.txt')
-    jsonData = parser.exportJson('data/data_json/data.json')
 
+    if not os.path.exists('data/data_json'):
+        os.makedirs('data/data_json')
+
+    jsonData = parser.exportJson('data/data_json/data.json')
     sampleExtractor = tweetcontainstagextractor.TweetContainsTagExtracor()
     sampleExtractor.loadJson('data/data_json/data.json')
     sampleExtractor.applyTransformation()
